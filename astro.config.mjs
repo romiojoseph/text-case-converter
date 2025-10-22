7509847cecf79aef237f5d5fdb257b5795686e61
romiojoseph <55487671+romiojoseph@users.noreply.github.com>
2025-10-22 23:27:57 +0530

    Config file updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://romiojoseph.github.io',
+    base: '/text-case-converter',
+});
