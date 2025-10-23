30c7c62b5cafb6a597306042b9fc6f5c94749eb3
romiojoseph <55487671+romiojoseph@users.noreply.github.com>
2025-10-23 10:34:51 +0530

    Fixed the known issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
+// astro.config.mjs
+
 // @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://romiojoseph.github.io',
-    base: '/text-case-converter',
+    // The base needs to be the full subfolder path
+    base: '/open-source/text-case-converter',
 });
